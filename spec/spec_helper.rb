f84d9258f1c7361c43285ccfca171805aa947794
William McCumsite <william.mccumstie@gmail.com>
2019-12-06 17:26:30 +0000

    Set a test token so it is consistent

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,7 @@
 #===============================================================================
 
 ENV['RACK_ENV'] = 'test'
+ENV['jwt_shared_secret'] = 'SOME_TEST_TOKEN'
 
 require 'rake'
 load File.expand_path('../Rakefile', __dir__)
